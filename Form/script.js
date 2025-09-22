a7e10b3f0f96b93fe1b96de57824bf921412d9bb
PaoloRobolini <nonilmiopsn@gmail.com>
2025-09-22 18:23:45 +0200

    FUNZIONA

--- Form/script.js
+++ Form/script.js
@@ -10,31 +10,17 @@ const datiUtente = {
 const submit = document.getElementById('submit')
 const divRisultato = document.getElementById('risultato')
 
-let messaggio = ""
 
-submit.onclick = () => {
-    for (const [key, value] of Object.entries(datiUtente)) {
-    messaggio += '${key}: ${value}<br>'
+submit.onclick = (event) => {
+    let messaggio = ""
+    event.preventDefault();
+    for(const key of Object.keys(datiUtente)) {
+        let value = document.getElementById(key).value.trim();
+        if (key === "newsletter") {
+        value = document.getElementById(key).checked ? "Sì" : "No";
+            }    
+        messaggio += `<b>${key}:</b> ${value}<br>`;
     }
-    divRisultato.innerText = messaggio
-    console.log(messaggio)
-    messaggio = ""
+        divRisultato.innerHTML = messaggio;
+     
 }
-/** 
- * document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('feedbackForm');
-    const divRisultato = document.getElementById('risultato');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        leggiDatiUtente();
-    let riepilogo = '<h2 style=\'font-weight:bold\'>Riepilogo del Feedback</h2>';
-    riepilogo += '<b>Nome:</b> ' + datiUtente.nome + '<br>';
-    riepilogo += '<b>Email:</b> ' + datiUtente.email + '<br>';
-    riepilogo += '<b>Data:</b> ' + datiUtente.data + '<br>';
-    riepilogo += '<b>Ora:</b> ' + datiUtente.ora + '<br>';
-    riepilogo += '<b>Messaggio:</b> ' + datiUtente.messaggio + '<br>';
-    riepilogo += '<b>Iscrizione alla newsletter:</b> ' + (datiUtente.newsletter === 'si' ? 'Sì' : 'No');
-        divRisultato.innerHTML = riepilogo;
-    });
-});
-*/
