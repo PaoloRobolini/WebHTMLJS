36dd1fe0b1b876b0ce4e4584fb21494dc5de9de3
PaoloRobolini <nonilmiopsn@gmail.com>
2025-12-03 15:38:19 +0100

    implementato cambio tema

--- controlla-spese/src/App.jsx
+++ controlla-spese/src/App.jsx
@@ -8,6 +8,26 @@ function App() {
 
   const [data, setData] = useState([])
   const [filteredData, setFilteredData] = useState([])
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark');
+
+  const THEMES = ['light', 'dark', 'cyberpunk', 'synthwave', 'winter'];
+
+  const handleThemeChange = (event) => {
+    const selectedTheme = event.target.value
+    setTheme(selectedTheme)
+    localStorage.setItem('theme', selectedTheme)
+    console.log(`Tema cambiato in: ${selectedTheme}`)
+  }  
+
+
+  useEffect(() => {
+    // Scrive il tema corrente su localStorage, salvandolo per la prossima visita
+    localStorage.setItem('theme', theme);
+
+    // Applica il tema al DOM
+    document.documentElement.setAttribute('data-theme', theme);
+
+  }, [theme]);
 
   const pb = new PocketBase('http://127.0.0.1:8090');
 
@@ -16,12 +36,13 @@ function App() {
     async function fetchData() {
 
       const fetchData = async () => {
-        const fetchData= await fetch("http://127.0.0.1:8090/api/collections/spese/records")
+        const fetchData = await fetch("http://127.0.0.1:8090/api/collections/spese/records")
         const response = await fetchData.json()
-        return response
+        return response.items
       }
 
       const resp = await fetchData()
+      console.log(resp)
       const listaProdotti = resp.map(item => ({
         nome: item.nome,
         descrizione: item.descrizione,
@@ -47,42 +68,42 @@ function App() {
 
 
   const handleAggiungiSpesa = async (nuovoAcquisto) => {
-        try {
-          const response = await fetch("http://127.0.0.1:8090/api/collections/spese/records", {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(nuovoAcquisto)
-          });
+    try {
+      const response = await fetch("http://127.0.0.1:8090/api/collections/spese/records", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(nuovoAcquisto)
+      });
 
-          // 2. Gestione della Risposta
-          if (!response.ok) {
-            const errorData = await response.json();
-            console.error("Errore durante l'invio a PocketBase:", errorData);
-            alert(`Errore nell'aggiunta: ${response.status} ${response.statusText}`);
-            return; 
-          }
+      // 2. Gestione della Risposta
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.error("Errore durante l'invio a PocketBase:", errorData);
+        alert(`Errore nell'aggiunta: ${response.status} ${response.statusText}`);
+        return;
+      }
 
-          document.getElementById("formSpesa").reset();
+      document.getElementById("formSpesa").reset();
 
 
-          // Crea Record preso da PocketBase
-          const recordCreato = await response.json();
+      // Crea Record preso da PocketBase
+      const recordCreato = await response.json();
 
-          console.log(`Creato il record ${JSON.stringify(recordCreato)}`);
-          // 3. Aggiorna data
-          setData(prevData => [recordCreato, ...prevData]);
-          console.log(`Nuovi dati: ${JSON.stringify(data)}`);
-          // 4. Chiude il Modale
-          document.getElementById("modaleAggiungiSpesa").close();
+      console.log(`Creato il record ${JSON.stringify(recordCreato)}`);
+      // 3. Aggiorna data
+      setData(prevData => [recordCreato, ...prevData]);
+      console.log(`Nuovi dati: ${JSON.stringify(data)}`);
+      // 4. Chiude il Modale
+      document.getElementById("modaleAggiungiSpesa").close();
 
-        } catch (error) {
-          //Errori vari perché quando mai qualcosa va bene
-          console.error("Errore di rete o catch finale:", error);
-          alert("Impossibile connettersi al server PocketBase.");
-        }
-    
+    } catch (error) {
+      //Errori vari perché quando mai qualcosa va bene
+      console.error("Errore di rete o catch finale:", error);
+      alert("Impossibile connettersi al server PocketBase.");
+    }
+
 
   };
 
@@ -110,7 +131,7 @@ function App() {
             Aggiungi Spesa
           </button>
 
-          {/* MODALE CORRETTO */}
+          {/* MODALE AGGIUNTA */}
           <dialog id="modaleAggiungiSpesa" className="modal" >
             <div className="modal-box"> {/* Rimosso justify-right, non necessario */}
 
@@ -176,17 +197,48 @@ function App() {
             </div>
           </dialog>
 
+          {/* MODALE FILTRO */}
           <dialog id="modaleFiltro" className="modal">
             <div id="modal-box">
-              <form id ="formFiltro">
-                  <h3 className="font-bold text-lg mb-4">Aggiungi una nuova spesa</h3>
+              <form id="formFiltro">
+                <h3 className="font-bold text-lg mb-4">Aggiungi una nuova spesa</h3>
               </form>
             </div>
           </dialog>
 
-          <button className="btn btn-secondary btn-sm m-2" onClick={()=>{
+          <button className="btn btn-secondary btn-sm m-2" onClick={() => {
             document.getElementById("modaleFiltro").showModal()
           }}>Filtra per...</button>
+
+          <div className="dropdown mb-72 rounded-border">
+            <div tabIndex={0} role="button" className="btn m-1">
+              Theme
+              <svg
+                width="12px"
+                height="12px"
+                className="inline-block h-2 w-2 fill-current opacity-60"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 2048 2048">
+                <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+              </svg>
+            </div>
+            {/*SCELTA TEMI*/}
+            <ul tabIndex="-1" className="dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl"
+            onChange={handleThemeChange}
+            >
+
+              {THEMES.map((thm) => (
+                <li key={thm}>
+                  <input
+                    type="radio"
+                    name="theme-dropdown"
+                    className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+                    aria-label={thm}
+                    value={thm} />
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
       {/* {<div id ="riempi" className = "card pb-40px" ></div>} */}
